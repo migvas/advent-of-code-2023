5987cbcd30a76566a507e19a6104b59ba16b2751
migvas <migas.v94@gmail.com>
2023-12-27 21:57:20 +0000

    fix: Part2 now runs

--- day23/part2.py
+++ day23/part2.py
@@ -2,7 +2,7 @@ import sys
 
 sys.setrecursionlimit(10000)
 directions = [(0,1), (1,0), (0, -1), (-1, 0)]
-
+visited_nodes = {}
 class Graph:
     def __init__(self):
         self.nodes = {}
@@ -11,19 +11,29 @@ class Graph:
         self.nodes[node] = []
 
     def add_edge(self, parent_node, node, steps):
+        if parent_node not in self.nodes:
+            self.add_node(parent_node)
+        if node not in self.nodes:
+            self.add_node(node)
+        
+
         self.nodes[parent_node].append({
             "node": node,
             "steps": steps
         })
 
-def build_graph(x, y, steps, visited, parent_node):
-    print((x,y))
+        self.nodes[node].append({
+            "node": parent_node,
+            "steps": steps
+        })
+
+def build_graph(x, y, steps, parent_node):
+    global visited_nodes
     
     if (x,y) == starting_pos:
-        G.add_node((x, y))
         parent_node = (x, y)
 
-    visited.append((x,y))
+    visited_nodes[(x,y)] = True
     if (x,y) == finish:
         G.add_edge(parent_node, (x,y), steps)
         return
@@ -33,18 +43,19 @@ def build_graph(x, y, steps, visited, parent_node):
     for d in directions:
         new_x = x + d[0]
         new_y = y + d[1]
-        if grid[new_x][new_y] != "#" and (new_x, new_y) not in visited:
+        if grid[new_x][new_y] != "#" and (new_x, new_y) not in visited_nodes:
                 if new_x >= 0 and new_x < len(grid) and new_y >= 0 and new_y < len(grid[0]):
                     next_pos.append((new_x, new_y))
+        elif (new_x, new_y) in G.nodes and (new_x, new_y) != parent_node:
+            G.add_edge(parent_node, (new_x, new_y), steps + 1)
 
     if len(next_pos) > 1:
         G.add_edge(parent_node, (x,y), steps)
-        G.add_node((x,y))
         parent_node = (x,y)
         steps = 0
     
     for p in next_pos:
-        build_graph(p[0], p[1], steps + 1, list(visited), parent_node)
+        build_graph(p[0], p[1], steps + 1, parent_node)
     
     return
     
@@ -84,6 +95,6 @@ for i, line in enumerate(data):
 
 G = Graph()
 
-build_graph(starting_pos[0], starting_pos[1], 0, [], None)
+build_graph(starting_pos[0], starting_pos[1], 0, None)
 print("Graph done")
 print(dfs(starting_pos[0], starting_pos[1], 0, []))
